3e331abc20a3ac846db6580015b9bab2a5e187a1
Soumyaranjan Sahoo (NTE-492) <soumyaranjansahoo@nte-492-mac.local>
2024-09-05 00:28:14 +0530

    fixed the file path isues

--- src/MentorDetails/AddQuestions.jsx
+++ src/MentorDetails/AddQuestions.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import "../MentorDetails/addquestion.css";
 
 const AddQuestions = () => {
+  const [isQuestionVisible, setIsQuestionVisible] = useState(false);
+  const toggleQuestionVisibility = () => {
+    setIsQuestionVisible((prevState) => !prevState);
+  };
   return (
-    <div>AddQuestions</div>
-  )
-}
+    <div className="question-page">
+      <div className="question-container">
+        <p className="top-left-text2">Setup Preparing question</p>
+        {isQuestionVisible && (
+          <div className="question-section">
+            <div className="question-area">
+              <p>question1</p>
+              <input type="checkbox" /> <p>required</p>
+              <DeleteTwoToneIcon />
+            </div>
 
-export default AddQuestions
+            <textarea className="text-area"></textarea>
+          </div>
+        )}
+
+        <div className="centered-content">
+          <button className="plus-button" onClick={toggleQuestionVisibility}>
+            <ControlPointIcon />
+          </button>
+          <p className="button-text">Add question</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddQuestions;
