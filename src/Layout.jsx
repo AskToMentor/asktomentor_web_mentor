8792e53758119f60bd8a02da4c4fd3a9637f5930
Soumyaranjan Sahoo (NTE-492) <soumyaranjansahoo@nte-492-mac.local>
2024-09-05 00:42:53 +0530

    fixed the navbar issues

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,11 +4,11 @@ import Navbar from "./common-components/Navbar";
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-row h-screen w-screen overflow-hidden">
-      <div className="main-content flex flex-col w-full h-full">
-        <div className="h-[80px] sm:h-[100px]">
+      <div className="main-content flex flex-col w-full h-screen overflow-hidden">
+        <div className="h-[10%]">
           <Navbar />
         </div>
-        <div className="w-full h-full overflow-hidden">{children}</div>
+        <div className="w-full h-[90%] overflow-hidden pb-2">{children}</div>
       </div>
     </div>
   );
