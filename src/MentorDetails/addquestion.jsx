8849baec7263bff90ce060e75230bea6aa19b7e7
Soumyaranjan Sahoo (NTE-492) <soumyaranjansahoo@nte-492-mac.local>
2024-09-05 00:04:57 +0530

    added the mobile design

--- src/MentorDetails/addquestion.jsx
+++ src/MentorDetails/addquestion.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import "../MentorDetails/addquestion.css";
 
 const addquestion = () => {
+  const [isQuestionVisible, setIsQuestionVisible] = useState(false);
+  const toggleQuestionVisibility = () => {
+    setIsQuestionVisible(prevState => !prevState);
+  };
   return (
-    <div>addquestion</div>
+    <div className='question-page'>
+    <div className='question-container'>
+      <p className='top-left-text2'>Setup Preparing question</p>
+      {isQuestionVisible && (
+        <div className='question-section'>
+          <div className='question-area'>
+          <p>question1</p>
+          <input type='checkbox' /> <p>required</p>
+          <DeleteTwoToneIcon />
+          </div>
+          
+          <textarea className='text-area'></textarea>
+        </div>
+      )}
+
+      <div className='centered-content'>
+        <button className='plus-button' onClick={toggleQuestionVisibility}>
+          <ControlPointIcon />
+        </button>
+        <p className='button-text'>Add question</p>
+      </div>
+    </div>
+     
+  </div>
   )
 }
 
