4ab43676cc0ce96923ca56b642b346aacdfef6df
Satya <sjati1997@gmail.com>
2024-09-27 02:00:07 +0530

    BlogCSS

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -14,16 +14,17 @@ const Blogs = () => {
       <div className="w-full h-auto lg:h-[700px] mx-auto grid grid-cols-1 sm:grid-cols-2 items-center px-10 my-8">
         {/* Left Section - Heading */}
         <div className="text-left w-full pt-10 px-10">
-          <h1 className="common-text-linear-gradient italic text-[74px] font-normal leading-[87.74px] text-transparent bg-clip-text bg-gradient-to-r from-[#D3D9D5] to-[#124E66] opacity-0">
+          <h1 className="blog-text-linear-gradient italic text-[74px] sm:text-[80px] md:text-[90px] lg:text-[100px] font-normal leading-[87.74px] sm:leading-[94.94px] md:leading-[100px] text-transparent bg-clip-text bg-gradient-to-r from-[#D3D9D5] to-[#124E66] opacity-0">
             <b className="block">Empower Your</b>
             <b className="block">Learning Journey</b>
           </h1>
 
           {/* Paragraph */}
-          <p className="font-medium text-[28px] leading-[35.2px] text-left opacity-0 mt-10">
+          <p className="font-medium text-[28px] sm:text-[30px] md:text-[32px] leading-[35.2px] sm:leading-[38px] md:leading-[40px] text-left opacity-0 mt-10">
             Unlock the potential within you with mentorship stories, tips, and guidance from experienced professionals.
           </p>
         </div>
+
 
         {/* Right Section */}
         <div className="flex h-full w-full items-center justify-center sm:justify-end bg-transparent">
